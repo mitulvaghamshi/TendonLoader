b4ea0ceb22d77b07e305044a02ae552c417a1817
Mitul Vaghamshi <mitulvaghmashi@gmail.com>
2021-05-11 21:21:51 -0400

    compact web portal design

---
+++ lib/web/handler/item_click_handler.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:tendon_loader/shared/modal/chartdata.dart';
+import 'package:tendon_loader/shared/modal/prescription.dart';
+import 'package:tendon_loader/shared/modal/session_info.dart';
+
+@immutable
+class ItemClickHandler {
+  const ItemClickHandler({this.dataList, this.sessionInfo, this.prescription});
+
+  final List<ChartData> dataList;
+  final SessionInfo sessionInfo;
+  final Prescription prescription;
+}
--- lib/web/line_graph.dart
+++ lib/web/line_graph.dart
@@ -2,18 +2,14 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:tendon_loader/shared/constants.dart';
 import 'package:tendon_loader/shared/custom/custom_frame.dart';
+import 'package:tendon_loader/shared/custom/custom_image.dart';
 import 'package:tendon_loader/shared/modal/chartdata.dart';
-import 'package:tendon_loader/shared/modal/prescription.dart';
-import 'package:tendon_loader/shared/modal/session_info.dart';
+import 'package:tendon_loader/web/handler/item_click_controller.dart';
+import 'package:tendon_loader/web/handler/item_click_handler.dart';
 
 class LineGraph extends StatefulWidget {
-  const LineGraph({Key key, this.data, this.sessionInfo, this.prescription}) : super(key: key);
-
-  final List<ChartData> data;
-  final SessionInfo sessionInfo;
-  final Prescription prescription;
+  const LineGraph({Key key}) : super(key: key);
 
   @override
   _LineGraphState createState() => _LineGraphState();
@@ -21,106 +17,113 @@ class LineGraph extends StatefulWidget {
 
 class _LineGraphState extends State<LineGraph> {
   final ScrollController _scrollController = ScrollController();
+  final TextStyle ts14 = const TextStyle(fontSize: 14);
 
   @override
   Widget build(BuildContext context) {
-    final bool _useDrawer = MediaQuery.of(context).size.width < Sizes.SIZE_MIN_WIDTH;
-    const TextStyle ts14 = TextStyle(fontSize: 14);
-    final LimitedBox _dataTable = LimitedBox(
-      maxWidth: 200,
-      child: Scrollbar(
-        thickness: 15,
-        isAlwaysShown: true,
-        controller: _scrollController,
-        child: ListView.builder(
-          itemExtent: 50,
-          controller: _scrollController,
-          itemCount: widget.data.length,
-          physics: const AlwaysScrollableScrollPhysics(),
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          itemBuilder: (_, int index) => ListTile(
-            leading: Text('${index + 1})', style: ts14),
-            title: Text('${widget.data[index].time}', style: ts14),
-            trailing: Text('${widget.data[index].load}', style: ts14),
-            tileColor: index.isEven ? Colors.grey.withOpacity(0.3) : null,
-          ),
-        ),
-      ),
-    );
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.sessionInfo.fileName)),
-      endDrawer: _useDrawer ? Drawer(child: _dataTable) : null,
-      body: AppFrame(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(children: <Widget>[widget.sessionInfo.toTable(), widget.prescription?.toTable()]),
-                  ),
-                  Expanded(
-                    child: SfCartesianChart(
-                      plotAreaBorderWidth: 0,
-                      tooltipBehavior: TooltipBehavior(
-                        enable: true,
-                        animationDuration: 0,
-                        header: widget.prescription != null ? 'Measurement' : 'MVC',
-                      ),
-                      primaryXAxis: NumericAxis(
-                        labelFormat: '{value} s',
-                        majorGridLines: const MajorGridLines(width: 0),
-                      ),
-                      primaryYAxis: NumericAxis(
-                        labelFormat: '{value} kg',
-                        anchorRangeToVisiblePoints: true,
-                        axisLine: const AxisLine(width: 0),
-                        majorTickLines: const MajorTickLines(size: 0),
-                        majorGridLines: MajorGridLines(color: Theme.of(context).accentColor),
-                      ),
-                      zoomPanBehavior: ZoomPanBehavior(
-                        enablePanning: true,
-                        enablePinching: true,
-                        zoomMode: ZoomMode.x,
-                        enableMouseWheelZooming: true,
-                      ),
-                      series: <ChartSeries<ChartData, double>>[
-                        AreaSeries<ChartData, double>(
-                          borderWidth: 1,
-                          dataSource: widget.data,
+    return AppFrame(
+      child: StreamBuilder<ItemClickHandler>(
+        stream: ItemClickController.stream,
+        builder: (_, AsyncSnapshot<ItemClickHandler> snapshot) {
+          if (!snapshot.hasData) return const Expanded(child: Center(child: CustomImage(isBg: true)));
+          return Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: <Widget>[
+                        snapshot.data.sessionInfo.toTable(),
+                        snapshot.data.prescription?.toTable(),
+                      ]),
+                    ),
+                    Expanded(
+                      child: SfCartesianChart(
+                        plotAreaBorderWidth: 0,
+                        tooltipBehavior: TooltipBehavior(
+                          enable: true,
                           animationDuration: 0,
-                          borderColor: Colors.black,
-                          xValueMapper: (ChartData data, _) => data.time,
-                          yValueMapper: (ChartData data, _) => data.load,
-                          gradient: const LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            stops: <double>[0.2, 0.8],
-                            colors: <Color>[Colors.blue, Colors.indigo],
-                          ),
+                          header: snapshot.data.prescription != null ? 'Measurement' : 'MVC',
                         ),
-                        if (widget.prescription != null)
-                          LineSeries<ChartData, double>(
-                            width: 2,
-                            color: Colors.red,
+                        primaryXAxis: NumericAxis(
+                          labelFormat: '{value} s',
+                          majorGridLines: const MajorGridLines(width: 0),
+                        ),
+                        primaryYAxis: NumericAxis(
+                          labelFormat: '{value} kg',
+                          anchorRangeToVisiblePoints: true,
+                          axisLine: const AxisLine(width: 0),
+                          majorTickLines: const MajorTickLines(size: 0),
+                          majorGridLines: MajorGridLines(color: Theme.of(context).accentColor),
+                        ),
+                        zoomPanBehavior: ZoomPanBehavior(
+                          enablePanning: true,
+                          enablePinching: true,
+                          zoomMode: ZoomMode.x,
+                          enableMouseWheelZooming: true,
+                        ),
+                        series: <ChartSeries<ChartData, double>>[
+                          AreaSeries<ChartData, double>(
+                            borderWidth: 1,
                             animationDuration: 0,
-                            yValueMapper: (ChartData data, _) => data.load,
+                            borderColor: Colors.black,
+                            dataSource: snapshot.data.dataList,
                             xValueMapper: (ChartData data, _) => data.time,
-                            dataSource: <ChartData>[
-                              ChartData(load: widget.prescription.targetLoad),
-                              ChartData(time: widget.data.last.time, load: widget.prescription.targetLoad),
-                            ],
+                            yValueMapper: (ChartData data, _) => data.load,
+                            gradient: const LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              stops: <double>[0.2, 0.8],
+                              colors: <Color>[Colors.blue, Colors.indigo],
+                            ),
                           ),
-                      ],
+                          if (snapshot.data.prescription != null)
+                            LineSeries<ChartData, double>(
+                              width: 2,
+                              color: Colors.red,
+                              animationDuration: 0,
+                              yValueMapper: (ChartData data, _) => data.load,
+                              xValueMapper: (ChartData data, _) => data.time,
+                              dataSource: <ChartData>[
+                                ChartData(load: snapshot.data.prescription.targetLoad),
+                                ChartData(
+                                  time: snapshot.data.dataList.last.time,
+                                  load: snapshot.data.prescription.targetLoad,
+                                ),
+                              ],
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              if (snapshot.data.prescription != null)
+                LimitedBox(
+                  maxWidth: 200,
+                  child: Scrollbar(
+                    thickness: 15,
+                    isAlwaysShown: true,
+                    controller: _scrollController,
+                    child: ListView.builder(
+                      itemExtent: 50,
+                      controller: _scrollController,
+                      itemCount: snapshot.data.dataList.length,
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      itemBuilder: (_, int index) => ListTile(
+                        leading: Text('${index + 1})', style: ts14),
+                        title: Text('${snapshot.data.dataList[index].time}', style: ts14),
+                        trailing: Text('${snapshot.data.dataList[index].load}', style: ts14),
+                        tileColor: index.isEven ? Colors.grey.withOpacity(0.3) : null,
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
-            if (!_useDrawer) _dataTable
-          ],
-        ),
+                ),
+            ],
+          );
+        },
       ),
     );
   }
