1110c6e07ab04c0b92745aed96377cf51711a1e9
mitulvaghamshi <mitulvaghmashi@gmail.com>
2023-05-04 21:16:19 -0400

    add generated files to version controll

---
+++ lib/modal/settings_state.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_state.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SettingsStateAdapter extends TypeAdapter<SettingsState> {
+  @override
+  final int typeId = 5;
+
+  @override
+  SettingsState read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SettingsState(
+      autoUpload: fields[0] as bool?,
+      graphSize: fields[1] as double?,
+      customPrescriptions: fields[2] as bool?,
+      mvcDuration: fields[3] as int?,
+      prescription: fields[4] as Prescription?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, SettingsState obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.autoUpload)
+      ..writeByte(1)
+      ..write(obj.graphSize)
+      ..writeByte(2)
+      ..write(obj.customPrescriptions)
+      ..writeByte(3)
+      ..write(obj.mvcDuration)
+      ..writeByte(4)
+      ..write(obj.prescription);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingsStateAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
